dc2e492772ad227792019bdc8bc88e2f5b5e8144
AMMAR721 <95668536+Ammar72H@users.noreply.github.com>
2022-08-28 07:45:05 +0200

    search setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:news/modules/home/home_screen.dart';
+import 'package:news/providers/my_provider.dart';
 import 'package:news/shared/styles/my_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp( MyApp());
---
+++ lib/modules/home/search.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(50),
+              bottomLeft: Radius.circular(50),
+            ),
+          ),
+        toolbarHeight: 90,
+        iconTheme: Theme.of(context).iconTheme,
+        // The search area here
+          title: Container(
+            width: double.infinity,
+            height: 40,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(5)),
+            child: Center(
+              child: TextField(
+                decoration: InputDecoration(
+                    prefixIcon:IconButton(
+                        icon: const Icon(Icons.search),
+                      onPressed: () {
+
+                      },
+                    ),
+
+                    suffixIcon: IconButton(
+                      icon: const Icon(Icons.clear,color: Colors.black,),
+                      onPressed: () {
+                        /* Clear the search field */
+                      },
+                    ),
+                    hintText: 'Search...',
+                    hintStyle: TextStyle(color: Colors.black,fontSize: 15),
+                    border: InputBorder.none
+                ),
+              ),
+            ),
+          )),
+       backgroundColor: Theme.of(context).backgroundColor,
+    );
+  }
+}
